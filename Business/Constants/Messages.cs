e89456777cd84e1945e019e5ba54ff216226bea5
baran <baran@DESKTOP-NTI5MFB>
2022-06-13 10:50:02 +0300

    10. gun sonu bruderim

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "urun eklendi";
+        public static string ProductNameInvalid = "urun ismi gecersiz";
+        public static string MainTenanceTime ="sistem bakimda" ;
+        public static string ProductsListed = "urunler listelendi";
+    }
+}
